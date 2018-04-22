d010d2ab46ceb7e61c33efda112cd476332d8016
ToraSonic <choiwenting@gmail.com>
2018-04-22 20:15:52 +0800

    finish first project

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -147,7 +147,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 3000
-learning_rate = 0.1
-hidden_nodes = 28
+iterations = 5000
+learning_rate = 0.5
+hidden_nodes = 30
 output_nodes = 1
